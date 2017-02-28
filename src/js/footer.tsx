6c32470004a71b711d256ea0f500d7911b6d2df0
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-03-01 12:01:22 +1300

    added promo

--- src/js/footer.tsx
+++ src/js/footer.tsx
@@ -3,13 +3,12 @@ import * as React from "react";
 export default class Footer extends React.Component<{}, {}> {
     render(){
     return <div className="footer">
-    <div className="container">
-        <div className="footer-bar">
-        <a href="https://browser.catalex.nz" className="browser-logo"></a>
-        <div className="catalex-logo"></div>
-        <a href="https://catalex.nz">© 2016 CataLex®</a>
+        <div className="container">
+            <div className="footer-bar">
+            <div className="catalex-logo"></div>
+            <a href="https://catalex.nz">© {(new Date()).getFullYear()} CataLex®</a>
+            </div>
         </div>
-    </div>
     </div>
     }
 }
